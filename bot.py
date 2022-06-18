26436134f52911a5364cfac28484375ea8dd670b
Joyce Lu <48322697+pumpjoy@users.noreply.github.com>
2022-06-18 14:11:11 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,47 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.event
+async def on_message(message):
+    # channel = client.get_channel(986162355698294834)
+    # await channel.send('hello')
+
+    if message.author == bot.user:
+        return
+
+    if message.content == 'hey bot':
+        await message.channel.send('what u want')
+
+    if message.content == 'bot you seeing this?':
+        await message.channel.send('what where')
+    
+    await bot.process_commands(message)
+
+foo = 0
+@bot.command()
+async def answeris(ctx):
+    global foo
+    await ctx.send(foo)
+    foo += 1
+
+
+@bot.command()
+async def test(ctx, arg1, arg2):
+    await ctx.send('You passed {} and {}'.format(arg1, arg2))
+
+# bot.add_command(answeris)
+# bot.add_command(test)
+bot.run(TOKEN)
