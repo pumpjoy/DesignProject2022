cb5723c8bbe0d4a5024803a8ab77c085fe9b6214
Joyce Lu <48322697+pumpjoy@users.noreply.github.com>
2022-06-18 14:13:34 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,9 +5,11 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
-import eyetrack as eye
-bar = ''
 
+""" custom opencv eyetracking libraries"""
+import eyetrack4 as eye
+
+""" main functions """
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -39,15 +41,39 @@ async def increase(ctx):
     await ctx.send(foo)
     foo += 1
 
-
 @bot.command()
 async def test(ctx, arg1, arg2):
     await ctx.send('You passed {} and {}'.format(arg1, arg2))
 
+"""the real deal is here"""
+camis = 0
+anotherfoo = 'Built-in cam'
+@bot.command()
+async def changecam(ctx, arg1: int):
+    global camis
+    global anotherfoo 
+
+    if arg1 == camis:
+        await ctx.send(f'Configuration is already {camis} which is {anotherfoo}')
+        return
+
+    if arg1 != 0 and arg1 != 1:
+        await ctx.send('Only 0 or 1 is accepted. 0 for Built-in cam, 1 for External cam 1.')
+        return
+    else:
+        camis = arg1
+        if camis == 0:
+            anotherfoo = 'Built-in cam'
+        elif camis == 1:
+            anotherfoo = 'External cam 1' 
+
+        await ctx.send(f'Done. Cam is set to {camis}, {anotherfoo}')
+        await ctx.send("You seeing this?")
+
 @bot.command()
 async def result(ctx):
-    global bar
-    bar = eye.returnvalue()
+    global camis
+    bar = eye.returnvalue(camis)
     await ctx.send(bar)
 
 bot.run(TOKEN)
